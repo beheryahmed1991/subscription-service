b0ea6100dd841b57938ac181b7089927f41d120b
behery_ahmed <behery.ahmed1991@gmail.com>
2025-11-10 12:24:17 +0300

    add doc for swagger

--- server/subscription/main.go
+++ server/subscription/main.go
@@ -3,17 +3,24 @@ package main
 import (
 	"fmt"
 
+	_ "github.com/beheryahmed1991/subscription-service.git/server/subscription/docs"
 	"github.com/gin-gonic/gin"
-
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
-
+// @title Subscription Service
+// @version 1.0
+// @description REST API for managing user subscriptions
+// @host localhost:8080
 func main() {
 	router := gin.Default()
 
 	router.GET("/hello", func(c *gin.Context) {
 		c.String(200, "Hello, ahmed!")
 	})
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	if err := router.Run(":8080"); err != nil {
 		fmt.Println("Failed to start server:", err)
 	}
